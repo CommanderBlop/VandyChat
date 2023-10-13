e198f418292053983cea8dc8c6c6358bcb41641a
Jack Long <kcajlong@outlook.com>
2023-10-13 16:15:58 -0500

    update hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
