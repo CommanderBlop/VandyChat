39aa58d2f4626cbd635533b17bb4d0fc48168549
Jack Long <kcajlong@outlook.com>
2023-10-23 13:53:57 -0500

    firebase connection and login (#2)
    * firebase init implementation
    
    * login with firebase

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app';
+// import 'firebase/auth';
+import 'firebase/firestore';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+  
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+export const auth = getAuth();
+export const googleAuthProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleAuthProvider);
+      return result.user;
+    } catch (error) {
+      console.error("Error during Google sign in", error);
+      return null;
+    }
+  };  
+
+export const db = getFirestore(app);
