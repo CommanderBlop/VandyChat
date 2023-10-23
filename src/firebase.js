344a107a65ea539946ad655da457bef8785f2de2
Jack Long <kcajlong@outlook.com>
2023-10-23 13:48:45 -0500

    login with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,21 @@ import { initializeApp } from 'firebase/app';
 import 'firebase/firestore';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
   
   const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 export const auth = getAuth();
 export const googleAuthProvider = new GoogleAuthProvider();
