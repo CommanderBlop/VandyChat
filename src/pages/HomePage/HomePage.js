5992cf8eaab754f84ba43a3f357b1d19826a6a3c
Jack Long <kcajlong@outlook.com>
2023-12-11 14:20:09 -0600

    updates

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
-import '../../global.css';
+// import '../../global.css';
 
 const HomePage = () => {
   return (
